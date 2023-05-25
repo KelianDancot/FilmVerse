e949dc2aa180ae12297bdb4cd5368740fe4bc745
Kélian <kelian.dancot@epsi.fr>
2023-05-25 14:46:00 +0200

    :art: style header

--- inc/header.php
+++ inc/header.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>PhotoFolio Bootstrap Template - Index</title>
+  <title>FilmVerse</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -34,18 +34,16 @@
   <!-- ======= Header ======= -->
   <header id="header" class="header d-flex align-items-center fixed-top">
     <div class="container-fluid d-flex align-items-center justify-content-between">
-
       <a href="index.html" class="logo d-flex align-items-center  me-auto me-lg-0">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <!-- <img src="assets/img/logo.png" alt=""> -->
-        <i class="bi bi-camera"></i>
-        <h1>PhotoFolio</h1>
+        <i class="bi bi-camera-reels"></i>
+        <h1>FilmVerse</h1>
       </a>
 
       <nav id="navbar" class="navbar">
         <ul>
           <li><a href="index.html" class="active">Accueil</a></li>
-          <li><a href="about.html">Watchlist</a></li>
           <li class="dropdown"><a href="#"><span>Films</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
             <ul>
               <li><a href="gallery.html">Populaire</a></li>
@@ -56,14 +54,5 @@
           </li>
         </ul>
       </nav><!-- .navbar -->
-
-      <div class="header-social-links">
-        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
-      </div>
-      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
-      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
-
     </div>
   </header><!-- End Header -->
